efc2e9d3ce2fd1ea3312ce5d113946b22aed0998
David Bisrat <>
2023-06-03 23:55:52 -0400

    use local database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export const revalidate = 0;
 
 async function getUsers() {
 let { data: Users, error } = await supabase
-.from('Users')
+.from('players')
 .select('*')
 
   if (error) console.error('error', error)
@@ -14,10 +14,14 @@ let { data: Users, error } = await supabase
 
 export default async function Home() {
   const users = await getUsers();
+  console.log('users=', users)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <ul>
-        {users?.map((user, i) =>  <li key={i}>{user.user_name}</li>)}
+        {users?.map((user, i) => {
+          console.log(i, user)
+          return <li key={i}>{i}{user.id}</li>
+        })}
       </ul>
       <h1 className="text-4xl font-bold"> <a href="https://nextjs.org">Next.js!</a></h1>
     </main>
